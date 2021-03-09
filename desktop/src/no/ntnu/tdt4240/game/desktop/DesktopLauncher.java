6b568687c8067d4f0b2650e55aefa4441afa9b7f
William Andersson <wandersson.98@gmail.com>
2021-03-09 13:40:23 +0100

    init

---
+++ desktop/src/no/ntnu/tdt4240/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package no.ntnu.tdt4240.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import no.ntnu.tdt4240.game.StudentLifeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StudentLifeGame(), config);
+	}
+}
