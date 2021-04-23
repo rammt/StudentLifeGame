36d8b5eca00c8120b7f1939eef4e3c9cb3703962
Elias Larsen <eliaslarsen@dhcp-10-22-15-248.wlan.ntnu.no>
2021-04-23 18:18:21 +0200

    init

---
+++ core/src/no/ntnu/tdt4240/game/screens/SettingScreen.java
@@ -0,0 +1,50 @@
+package no.ntnu.tdt4240.game.screens;
+
+import com.badlogic.gdx.Screen;
+
+import no.ntnu.tdt4240.game.StudentLifeGame;
+
+public class SettingScreen implements Screen {
+
+    final StudentLifeGame game;
+
+    public SettingScreen(final StudentLifeGame game) {
+        this.game = game;
+        this.game.getStage().clear();
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
